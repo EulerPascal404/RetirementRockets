e690c6d8edba893ad76bea23807d2c46c7f03deb
dwijb16 <90794286+dwijb16@users.noreply.github.com>
2023-03-13 07:49:24 -0500

    added component but fr this time

---
+++ retirementRocket/app/components/MyButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+function MyButton({title, onPress, backColor,}) {
+   return (
+      <TouchableOpacity
+         onPress={onPress}
+         style={[styles.rectangle, {backgroundColor: backColor}]}>
+       <Text style={styles.text}>{title}</Text>
+       </TouchableOpacity>
+
+   );
+}
+
+const styles = StyleSheet.create({
+   rectangle: {
+   height: 50,
+   width: 200,
+   alignItems: 'center',
+   justifyContent: 'center',
+   margin: 10,
+   borderRadius: 10,
+ },
+ text: {
+   fontWeight: 'bold',
+   fontSize: 15,
+   color: 'white',
+ },
+})
+export default MyButton;
