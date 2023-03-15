4d9f27ec3fcdb47afb8d1ef974274a73d06ea5cd
dwijb16 <90794286+dwijb16@users.noreply.github.com>
2023-03-15 07:44:55 -0500

    Added Custom TextInput and TextInputSeparator components

---
+++ retirementRocket/app/components/Separator.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+
+
+function Separator({color}) {
+  return (
+     <View style={[styles.itemSeparator, {backgroundColor: color}]}/>
+  );
+}
+  const styles = StyleSheet.create({
+    itemSeparator: {
+    width: '100%',
+    height: 2,
+    backgroundColor: 'gray',
+    marginBottom: 10,
+  },
+});
+
+export default Separator;
