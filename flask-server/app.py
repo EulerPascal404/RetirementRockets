15cd0071470f267166ed3634907a20c7d7bbe044
EulerPascal404 <67972733+EulerPascal404@users.noreply.github.com>
2023-03-16 08:16:55 -0500

    12

--- flask-server/app.py
+++ flask-server/app.py
@@ -6,12 +6,12 @@ from flask_restful import Api, reqparse
 from flask_cors import CORS #comment this on deployment
 from HelloApiHandler import HelloApiHandler
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__, static_url_path='/app.js', static_folder='../')
 CORS(app) #comment this on deployment
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
 def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+    return send_from_directory(app.static_folder,'app.js')
 
 api.add_resource(HelloApiHandler, '/flask/hello')
