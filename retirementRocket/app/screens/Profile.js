3453f23594594e3b4282e6f39a695b03290b0884
CheTenn <90787087+CheTenn@users.noreply.github.com>
2023-04-05 08:19:31 -0500

    Added the first part of the profile screen

--- retirementRocket/app/screens/Profile.js
+++ retirementRocket/app/screens/Profile.js
@@ -3,15 +3,49 @@ import { Text, TextInput, StyleSheet, Button, View } from 'react-native';
 
 import MyButton from '../components/MyButton';
 import colors from '../config/colors';
+import ProfileSeparator from '../components/ProfileSeparator';
 
 export default function Profile({ navigation }) {
 
     return(
-        <View> 
-       
+        <View style = {styles.container}>
+          <View style = {styles.row}>
+            <Text style={[styles.text, {paddingRight: 176}, {marginTop: 20}]}> Age: </Text>
+              <TextInput
+                style={{height: 20, width: 100, paddingLeft: 78, fontSize: 18}}
+                defaultValue = {"21"}
+              />
+          </View>
+          <View style = {styles.centerContainer}>
+            <ProfileSeparator/>
+          </View>
+
         </View>
     )
 
 
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+      width: "100%",
+    },
+
+      text: {
+        color: 'black',
+        fontSize: 18,
+    },
+
+      row: {
+        flexDirection: 'row',
+        alignItems: 'space-between',
+        justifyContent: 'center',
+        width: "100%"
+    },
+  
+    centerContainer: {
+      width: "100%",
+      alignItems: 'center'
+    },
+  });
