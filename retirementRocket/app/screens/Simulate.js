a50063c02b0f20d484842996e0a8bc4ad7e5641b
Sid-Rao06 <90772839+Sid-Rao06@users.noreply.github.com>
2023-04-06 08:12:29 -0500

    GOT THE SIMULATE PAGE DONE AND GOT THE ANNOYING BAR GRAPH ON THE SCREEN

--- retirementRocket/app/screens/Simulate.js
+++ retirementRocket/app/screens/Simulate.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { Text, TextInput, StyleSheet, Button, View } from 'react-native';
-
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 
 export default function Simulate({ navigation }) {
     return (
           <View style={styles.container}>
 
-          <Text style={[{fontSize: 15, fontWeight: 'bold'}]}> Overview 
+        <Text style={[{fontSize: 15, fontWeight: 'bold'},{marginTop:0}]}> Overview 
         </Text>
 
             <View style={[styles.circle,{marginTop: 0}]} ></View>
@@ -15,9 +22,19 @@ export default function Simulate({ navigation }) {
             <Text style={[{fontSize: 15, fontWeight: 'bold'},{marginTop: 180}]}> Charts 
         </Text>
 
-            <View style={[styles.circle]} ></View>
+        
 
-            <Text style={[{fontSize: 15, fontWeight: 'bold'},{marginTop: 180}]}> News 
+
+
+
+            <View style={[styles.circle]} ></View>
+            <LineChart
+  data={data}
+  width={400}
+  height={220}
+  chartConfig={chartConfig}
+/>
+            <Text style={[{fontSize: 15, fontWeight: 'bold'},{marginTop: 0}]}> News 
         </Text>
 
             <View style={[styles.circle1]} ></View>
@@ -28,7 +45,7 @@ export default function Simulate({ navigation }) {
 }
 const styles = StyleSheet.create({
     container: {
-      backgroundColor: '#7CA1B4',
+      backgroundColor: 'white',
       flex: 1,
       alignItems: 'flex-end', 
       justifyContent: 'center', 
@@ -58,3 +75,23 @@ const styles = StyleSheet.create({
       backgroundColor: 'black'
     }
   });
+
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        data: [20, 45, 28, 80, 99, 43],
+      }
+    ],
+  };
+
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 3, // optional, default 3
+    barPercentage: 1,
+    useShadowColorFromDataset: false // optional
+  };
