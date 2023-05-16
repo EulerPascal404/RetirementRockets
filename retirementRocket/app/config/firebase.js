8a44cc232cfc3dd8e876dcfd2e9578a9e896b3b2
dwijb16 <90794286+dwijb16@users.noreply.github.com>
2023-05-16 07:35:00 -0500

    initial firestore commit- setData() isn't working, but it will be by the end of the school day today

--- retirementRocket/app/config/firebase.js
+++ retirementRocket/app/config/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import 'firebase/auth';
-import 'firebase/firestore';
+//import 'firebase/auth';
+//import 'firebase/firestore';
 import {getAuth} from "firebase/auth"
 import Constants from 'expo-constants';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,4 +22,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+//export const auth = getAuth(app);
+export const auth = getAuth();
